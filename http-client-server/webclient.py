fdc96d70ce203571a2889a55b29630b4c9e25c5c
rk00 <00romankolesnik@gmail.com>
2024-11-25 19:22:11 +0300

    some http-clnt-srv extensions

--- http-client-server/webclient.py
+++ http-client-server/webclient.py
@@ -1,14 +1,28 @@
 import socket
 import sys
 
-host= sys.argv[1]   # читаем имя хоста и порт 
+host= sys.argv[1]                                       # читаем имя хоста и порт 
 port = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # создаем объект сокета, первый параметр говорит что используем IPv4
 s.connect((host, port))                                 # второй - то что используем TCP
 
-request = "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n" % (host)    # стандартный хедер реквеста, в конце пустая строка
-encoded_request = request.encode("ISO-8859-1")                                  # стандартная веб кодировка
+print("Enter the payload type: ", end='')
+payload_type = input()
+print("Enter the payload: ", end='')
+payload = input()
+
+request = (
+    f"GET / HTTP/1.1\r\n"                                   # стандартный хедер реквеста
+    f"Host: {host}\r\n"
+    f"Content-Type: {payload_type}\r\n"
+    f"Content-Length: {len(payload)}\r\n"
+    f"Connection: close\r\n"
+    f"\r\n"
+    f"{payload}"                                            # добавляем payload
+)
+
+encoded_request = request.encode("ISO-8859-1")              # стандартная веб кодировка
 s.sendall(encoded_request)
 
 response = b""
--- http-client-server/webserver.py
+++ http-client-server/webserver.py
@@ -12,17 +12,40 @@ s.listen()
 
 while True:
     new_conn = s.accept()               # возвращает новый сокет в виде объекта socket и адрес клиента в виде (host, port)
+    print("Client connected address: %s:%i" % (new_conn[1][0], new_conn[1][1]))         # выведем адресс клиента
     new_socket = new_conn[0]
     
-    request = b''
-    while True:
+    data = b''                                                              # читаем пока не найдем пустую строку
+    while b'\r\n\r\n' not in data:
         chunk = new_socket.recv(4096)
-        request += chunk
-        if request.decode("ISO-8859-1").find('\r\n\r\n') != -1:     # тут мы не можем просто итерироваться до момента пока
-            break                                                   # получим пустую строку, это происходит только когда
-                                                                    # клиент оборвет соединение, но он ждет ответа                               
-    response = """HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello!"""
-                                                        # простейший ответ http сервера
+        data+=chunk
+    headers, _, payload = data.decode("ISO-8859-1").partition("\r\n\r\n")   # разбиваем на заголовок и тело
+    
+    content_length = 0                                                      # найдем ожидаему длину тела
+    for line in headers.split("\r\n"):
+        if line.lower().startswith("Content-Length:"):
+            content_length = int(line.split(":")[1].strip())
+
+    payload_len = len(payload)                                              # если фактическая длина тела меньше ожидаемой, то 
+    if payload_len < content_length:                                        # считаем еще, пока не получим полностью
+        while payload_len < content_length:
+            chunk = new_socket.recv(4096)
+            payload += chunk
+            payload_len = len(payload)
+    
+    request_method = headers.split()[0]
+    print("Client request method:", request_method)
+    print("Client payload:", payload)
+                                       
+    response = (
+        "HTTP/1.1 200 OK\r\n"                   # простейший ответ http сервера
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 6\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "Hello!"
+    )
+    
     new_socket.sendall(response.encode("ISO-8859-1"))
     new_socket.close()
     
